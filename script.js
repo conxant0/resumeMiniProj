4164766698cb3c8e2dea54dfb91c15b238c8228c
Jezreel Chad Lumbab <151835239+conxant0@users.noreply.github.com>
2026-06-19 13:29:07 +0800

    feat: accurate date and time

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const now = () => {
+  let date = new Date();
 
+  return date
+    .toLocaleString("en-US", {
+      // Custom Format
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    })
+    .replaceAll(",", "");
+};
+
+const dateTime = document.querySelector(".dateTime");
+dateTime.innerHTML = now();
+
+setInterval(() => {
+  dateTime.innerHTML = now(); //Refresh every minute
+}, 60000);
