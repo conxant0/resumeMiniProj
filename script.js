e208219428677aee9c799b7dea41d9d8f376e5e8
Jezreel Chad Lumbab <151835239+conxant0@users.noreply.github.com>
2026-06-20 21:39:20 +0800

    feat: added click event to folders

--- script.js
+++ script.js
@@ -20,3 +20,38 @@ dateTime.innerHTML = now();
 setInterval(() => {
   dateTime.innerHTML = now(); //Refresh every minute
 }, 60000);
+
+const aboutFolder = document.querySelector(".aboutFolder");
+const aboutWindow = document.querySelector(".aboutWindow");
+
+aboutFolder.addEventListener("click", () => {
+  aboutWindow.classList.toggle("hidden");
+});
+
+const skillsFolder = document.querySelector(".skillsFolder");
+const skillsWindow = document.querySelector(".skillsWindow");
+
+skillsFolder.addEventListener("click", () => {
+  skillsWindow.classList.toggle("hidden");
+});
+
+const educationFolder = document.querySelector(".educationFolder");
+const educationWindow = document.querySelector(".educationWindow");
+
+educationFolder.addEventListener("click", () => {
+  educationWindow.classList.toggle("hidden");
+});
+
+const projectsFolder = document.querySelector(".projectsFolder");
+const projectsWindow = document.querySelector(".projectsWindow");
+
+projectsFolder.addEventListener("click", () => {
+  projectsWindow.classList.toggle("hidden");
+});
+
+const contactFolder = document.querySelector(".contactFolder");
+const contactWindow = document.querySelector(".contactWindow");
+
+contactFolder.addEventListener("click", () => {
+  contactWindow.classList.toggle("hidden");
+});
