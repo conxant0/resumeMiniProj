e94993337271113f76750e1aa92b7baee334f63e
Jezreel Chad Lumbab <151835239+conxant0@users.noreply.github.com>
2026-06-21 12:26:38 +0800

    feat: added folder posiiton logic

--- script.js
+++ script.js
@@ -21,46 +21,69 @@ setInterval(() => {
   dateTime.innerHTML = now(); //Refresh every minute
 }, 60000);
 
+let openCount = 0; // Counts how many folders are open
+
+const positionWindow = (folderWindow) => {
+  folderWindow.style.top = 90 + openCount * 20 + "px";
+  folderWindow.style.left = 140 + openCount * 20 + "px";
+  openCount++;
+};
+
 const aboutFolder = document.querySelector(".aboutFolder");
 const aboutWindow = document.querySelector(".aboutWindow");
 
 aboutFolder.addEventListener("click", () => {
-  aboutWindow.classList.toggle("hidden");
+  if (aboutWindow.classList.contains("hidden")) {
+    positionWindow(aboutWindow);
+    aboutWindow.classList.remove("hidden");
+  }
 });
 
 const skillsFolder = document.querySelector(".skillsFolder");
 const skillsWindow = document.querySelector(".skillsWindow");
 
 skillsFolder.addEventListener("click", () => {
-  skillsWindow.classList.toggle("hidden");
+  if (skillsWindow.classList.contains("hidden")) {
+    positionWindow(skillsWindow);
+    skillsWindow.classList.remove("hidden");
+  }
 });
 
 const educationFolder = document.querySelector(".educationFolder");
 const educationWindow = document.querySelector(".educationWindow");
 
 educationFolder.addEventListener("click", () => {
-  educationWindow.classList.toggle("hidden");
+  if (educationWindow.classList.contains("hidden")) {
+    positionWindow(educationWindow);
+    educationWindow.classList.remove("hidden");
+  }
 });
 
 const projectsFolder = document.querySelector(".projectsFolder");
 const projectsWindow = document.querySelector(".projectsWindow");
 
 projectsFolder.addEventListener("click", () => {
-  projectsWindow.classList.toggle("hidden");
+  if (projectsWindow.classList.contains("hidden")) {
+    positionWindow(projectsWindow);
+    projectsWindow.classList.remove("hidden");
+  }
 });
 
 const contactFolder = document.querySelector(".contactFolder");
 const contactWindow = document.querySelector(".contactWindow");
 
 contactFolder.addEventListener("click", () => {
-  contactWindow.classList.toggle("hidden");
+  if (contactWindow.classList.contains("hidden")) {
+    positionWindow(contactWindow);
+    contactWindow.classList.remove("hidden");
+  }
 });
 
 const closeButton = document.querySelectorAll(".closeButton");
 
 closeButton.forEach((cButton) => {
   cButton.addEventListener("click", () => {
-    const cWindow = cButton.closest(".folderWindow");
-    cWindow.classList.toggle("hidden");
+    const cWindow = cButton.closest(".folderWindow"); // Find closest window and close that one
+    cWindow.classList.add("hidden");
   });
 });
