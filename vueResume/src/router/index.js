fe97f49062dafd3d3ce0a0aff6ca0ef3cc46b27f
Jezreel Chad Lumbab <151835239+conxant0@users.noreply.github.com>
2026-07-19 12:55:41 +0800

    feat: added /about route

--- vueResume/src/router/index.js
+++ vueResume/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Desktop from '../components/Desktop.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Desktop',
     component: Desktop
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
   }
 ]
 
---
+++ vueResume/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WindowChrome from "../components/WindowChrome.vue";
+import AboutWindow from "../components/windows/AboutWindow.vue";
+import { aboutSection } from "../data/portfolio";
+</script>
+
+<template>
+  <WindowChrome id="about" title="About Me" :x="140" :y="90" :z-index="3">
+    <AboutWindow v-bind="aboutSection" />
+  </WindowChrome>
+</template>
