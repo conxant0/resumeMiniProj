4539beeff62bd2015fa198c44b0846a492359448
Jezreel Chad Lumbab <151835239+conxant0@users.noreply.github.com>
2026-07-19 12:59:55 +0800

    feat: added contact route

--- vueResume/src/router/index.js
+++ vueResume/src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Desktop from '../components/Desktop.vue'
-import AboutView from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Desktop from "../components/Desktop.vue";
+import AboutView from "../views/AboutView.vue";
+import ContactView from "../views/ContactView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Desktop',
-    component: Desktop
+    path: "/",
+    name: "Desktop",
+    component: Desktop,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: AboutView
-  }
-]
+    path: "/about",
+    name: "About",
+    component: AboutView,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: ContactView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ vueResume/src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WindowChrome from "../components/WindowChrome.vue";
+import ContactWindow from "../components/windows/ContactWindow.vue";
+import { contactSection } from "../data/portfolio";
+</script>
+
+<template>
+  <WindowChrome id="contact" title="Contact Me" :x="140" :y="90" :z-index="3">
+    <ContactWindow v-bind="contactSection" />
+  </WindowChrome>
+</template>
