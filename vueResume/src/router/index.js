d8b5bbee092536482ea8401d99d3d725abf2be0f
Jezreel Chad Lumbab <151835239+conxant0@users.noreply.github.com>
2026-07-19 13:03:09 +0800

    feat: added education route

--- vueResume/src/router/index.js
+++ vueResume/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Desktop from "../components/Desktop.vue";
 import AboutView from "../views/AboutView.vue";
 import ContactView from "../views/ContactView.vue";
+import EducationView from "../views/EducationView.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/contact",
     name: "Contact",
     component: ContactView,
+  },
+  {
+    path: "/education",
+    name: "Education",
+    component: EducationView,
   },
 ];
 
---
+++ vueResume/src/views/EducationView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import WindowChrome from "../components/WindowChrome.vue";
+import EducationWindow from "../components/windows/EducationWindow.vue";
+import { educationSection } from "../data/portfolio";
+</script>
+
+<template>
+  <WindowChrome
+    id="education"
+    title="My Education"
+    :x="140"
+    :y="90"
+    :z-index="3"
+  >
+    <EducationWindow v-bind="educationSection" />
+  </WindowChrome>
+</template>
